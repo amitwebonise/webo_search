8283075b1a0c82385c3a052eb9a2cd96d94f84a8
unnati <unnatipatil1988@gmail.com>
2018-04-05 11:01:58 +0530

    Allow cross site url access

--- app/controllers/webo_search/application_controller.rb
+++ app/controllers/webo_search/application_controller.rb
@@ -1,5 +1,11 @@
 module WeboSearch
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    private
+
+    def allow_iframe
+      response.headers.delete 'X-Frame-Options'
+    end
   end
 end
--- app/controllers/webo_search/searches_controller.rb
+++ app/controllers/webo_search/searches_controller.rb
@@ -1,14 +1,15 @@
 module WeboSearch
   class SearchesController < ApplicationController
     before_action :extract_namespace
+    after_action :allow_iframe, only: :show
 
     def show
-        if @webo_search.has_key?('primary_model')
-          @results = WeboSearch.results(params) if params.has_key?('search')
-        else
-          Rails.logger.info('Please check webo_search.yml for proper configuration !!!')
-          redirect_to :back, alert: 'You cannot access this URL !!!'
-        end
+      if @webo_search.has_key?('primary_model')
+        @results = WeboSearch.results(params) if params.has_key?('search')
+      else
+        Rails.logger.info('Please check webo_search.yml for proper configuration !!!')
+        redirect_to :back, alert: 'You cannot access this URL !!!'
+      end
     end
 
     def extract_namespace
