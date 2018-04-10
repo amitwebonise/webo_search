1382d6320ac697aa028983a57f7954dc83adfc3b
unnati <unnatipatil1988@gmail.com>
2018-04-10 15:28:43 +0530

    Revert code for iframe

--- app/controllers/webo_search/searches_controller.rb
+++ app/controllers/webo_search/searches_controller.rb
@@ -1,8 +1,6 @@
 module WeboSearch
   class SearchesController < ApplicationController
-    layout :false
     before_action :extract_namespace
-    after_action :allow_iframe, only: :show
 
     def show
       if @webo_search.has_key?('primary_model')
@@ -19,12 +17,6 @@ module WeboSearch
         @webo_search['primary_model'] = @webo_search["#{params[:namespace]}"]['primary_model']
         @webo_search['associated_model'] = @webo_search["#{params[:namespace]}"]['associated_model']
       end
-    end
-
-    private
-
-    def allow_iframe
-      response.headers.delete 'X-Frame-Options'
     end
   end
 end
